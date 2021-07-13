334eac88a1965817c8e28e50108bc6520a77c7c7
panuwat0437 <panuwatcomputer2556@gmail.com>
2021-07-13 22:22:16 +0700

    34534
    3454

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useEffect,useState} from 'react'; //จัดการ state
+const liff = window.liff;
 
 function App() {
+  // const [name, setName] = useState("");
+  // const [userLineID, setUserLineID] = useState("");
+  // const [pictureUrl, setPictureUrl] = useState("");
+
+  const logout = () => {
+    liff.logout();
+    window.location.reload();
+  }
+
+  async function fetchLiff() {
+    await liff.init({ liffId: `1655557814-mPVknAg8` }).catch(err=>{throw err});
+      if (liff.isLoggedIn()) {
+        let getProfile =  await liff.getProfile();
+        // setName(getProfile.displayName);
+        // setUserLineID(getProfile.userId);
+        // setPictureUrl(getProfile.pictureUrl);
+        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSdH7uwMINodj3dJSPpss-_DnJIiQ4t_U0TnJ__rZDamfbDUdA/viewform?usp=pp_url&entry.862938837="+
+        getProfile.userId+"&entry.428673667="+getProfile.displayName+"&entry.845592098="+getProfile.pictureUrl;
+      }else{
+        
+        liff.login();
+      }
+  }
+
+  useEffect(() => {
+    fetchLiff();
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Lineliff</h1>
+     {/* <p>ชื่อ {name}</p>
+     <p>UserID {userLineID}</p>
+     <img alt='img' src={pictureUrl} width="300px"  /><br></br> */}
+     <button onClick={() => logout()} className="mt-2" style={{ width: "20%", height: 30 }}>Logout</button>
     </div>
   );
 }
